8be1996819149db918a0ec6bc8249aaa50f94250
Marc Plouhinec <m_plouhinec@yahoo.fr>
2021-06-05 12:22:58 +0800

    Build a draft simulation results panel with Chart.js.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 import {simulationConfigController} from './facade/controller/simulationConfigController.js';
+import {simulationResultsController} from './facade/controller/simulationResultsController.js';
 
 simulationConfigController.init();
+simulationResultsController.init();
