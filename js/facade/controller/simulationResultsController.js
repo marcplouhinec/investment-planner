f4b6b62f610e3671b415c23f452e657d2270c00c
Marc Plouhinec <m_plouhinec@yahoo.fr>
2021-06-06 16:05:17 +0800

    Fix the color palette for the portfolio asset weight line chart.

--- js/facade/controller/simulationResultsController.js
+++ js/facade/controller/simulationResultsController.js
@@ -5,6 +5,33 @@ import {PortfolioInvestment} from "../../model/PortfolioInvestment.js";
 
 const simulationResultsController = {
 
+    /**
+     * @constant
+     * @type {{backgroundColor:string, borderColor:string}[]}
+     */
+    _CHART_COLORS: [
+        {
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)'
+        },
+        {
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 1)'
+        },
+        {
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)'
+        },
+        {
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            borderColor: 'rgba(153, 102, 255, 1)'
+        },
+        {
+            backgroundColor: 'rgba(255, 159, 64, 0.2)',
+            borderColor: 'rgba(255, 159, 64, 1)'
+        }
+    ],
+
     /** @type {?Chart} */
     _assetWeightLineChart: null,
 
@@ -30,11 +57,12 @@ const simulationResultsController = {
         // Draw the asset weight line chart
         const assetWeightLineChartData = {
             labels: yearMonths.map(it => it.toString()),
-            datasets: config.portfolioInvestments.map(investment => {
+            datasets: config.portfolioInvestments.map((investment, index) => {
+                const chartColor = this._CHART_COLORS[index % this._CHART_COLORS.length];
                 return {
                     label: investment.assetCode,
-                    backgroundColor: 'rgb(255, 99, 132)', // TODO
-                    borderColor: 'rgb(255, 99, 132)', // TODO
+                    backgroundColor: chartColor.backgroundColor,
+                    borderColor: chartColor.borderColor,
                     data: yearMonths.map(it => investment.computeWeightAt(it))
                 };
             })
