1364bfa7e67a901edef53281ecdd879547178352
Marc Plouhinec <m_plouhinec@yahoo.fr>
2021-06-13 13:52:15 +0800

    Write a service that can calculate annualized performance with historical prices.

--- test-js/service/historicalPriceAnalysisServiceTest.js
+++ test-js/service/historicalPriceAnalysisServiceTest.js
@@ -17,10 +17,25 @@ describe('historicalPriceAnalysisService', () => {
             });
             const prices = await historicalPriceReadingService.readHistoricalPrices(asset);
 
-            // Calculate the annualized performance between for 2020
-            const performance = historicalPriceAnalysisService.calculateAnnualPerformance(
+            // Calculate the annualized performance for 2020
+            let performance = historicalPriceAnalysisService.calculateAnnualPerformance(
                 prices, new YearMonth('2020-01'), new YearMonth('2021-01'));
             assert.equal(performance, 0.19224284565945138);
+
+            // Calculate the annualized performance for 2019
+            performance = historicalPriceAnalysisService.calculateAnnualPerformance(
+                prices, new YearMonth('2019-01'), new YearMonth('2020-01'));
+            assert.equal(performance, 0.29071612450661855);
+
+            // Calculate the annualized performance between 2019 and 2020 (2 years)
+            performance = historicalPriceAnalysisService.calculateAnnualPerformance(
+                prices, new YearMonth('2019-01'), new YearMonth('2021-01'));
+            assert.equal(performance, 0.2405027469620169);
+
+            // Calculate the annualized performance for 2 years until 2020-05-01
+            performance = historicalPriceAnalysisService.calculateAnnualPerformance(
+                prices, new YearMonth('2019-05'), new YearMonth('2021-05'));
+            assert.equal(performance, 0.2029080283736311);
         });
     });
 });
